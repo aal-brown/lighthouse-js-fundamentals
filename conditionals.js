c78b7d4fea7668ba52177c2da59d38298e782a65
Adlai <aal.brown2@gmail.com>
2019-12-09 19:09:20 +0000

    Conditionals practice updated.

--- conditionals.js
+++ conditionals.js
@@ -10,3 +10,14 @@ console.log("Make sure you pick out a scarf!");
 }
 
 console.log("Now you're ready to go outside!");
+
+//I didn't include the other examples given in the activity, as I'm very comfortable with this already.
+
+//my own basic code sample from one of the udacity exercises.
+var eatsPlants = true;
+var eatsAnimals = true;
+
+var category = (((eatsPlants && !eatsAnimals) ? "herbivore" : ((!eatsPlants && eatsAnimals) ? "carnivore" : (eatsPlants && eatsAnimals) ? "omnivore" : "undefined")))
+
+console.log(category);
+
